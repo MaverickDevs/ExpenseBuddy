454a27e864868c86fe87ed24f68c81bec6e24490
Ashmn07 <ashwin.menon202@gmail.com>
2025-02-14 15:35:25 +0530

    Initial React Native and Spring boot setup

---
+++ backend/src/main/java/com/maverickdevs/expensebuddy/ExpensebuddyApplication.java
@@ -0,0 +1,13 @@
+package com.maverickdevs.expensebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensebuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensebuddyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/maverickdevs/expensebuddy/ExpensebuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maverickdevs.expensebuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensebuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
