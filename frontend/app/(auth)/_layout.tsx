801320ded75229d44418e9e1fc0ee081419ffce1
Ashwin Menon <56076713+Ashmn07@users.noreply.github.com>
2025-03-12 10:01:16 +0530

    Feature/setup bottom tabs (#22)
    * Added API logic and fixed bug
    
    * Added bottom tabs

--- frontend/app/(auth)/_layout.tsx
+++ frontend/app/(auth)/_layout.tsx
@@ -1,10 +1,21 @@
 import { Tabs } from 'expo-router';
+import Entypo from '@expo/vector-icons/Entypo';
 
 export default function AuthLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}  initialRouteName="signup">
-      <Tabs.Screen name="login" />
-      <Tabs.Screen name="signup" />
+    <Tabs screenOptions={{ headerShown: false }} >
+        <Tabs.Screen 
+          name="index"   
+          options={{
+          title: 'Sign Up',
+          tabBarIcon: ({ color }) => <Entypo name="add-user" size={24} color="black" />,
+        }}/>
+        <Tabs.Screen 
+          name="login"   
+          options={{
+          title: 'Login',
+          tabBarIcon: ({ color }) => <Entypo name="login" size={24} color="black" />
+        }}/>
     </Tabs>
   );
 }
