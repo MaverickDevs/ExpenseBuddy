16b369546f5ac7d4bc9333abf0c6665c66decbc3
Ashmn07 <ashwin.menon202@gmail.com>
2025-03-12 09:39:26 +0530

    Added bottom tabs

--- frontend/app/(main)/_layout.tsx
+++ frontend/app/(main)/_layout.tsx
@@ -1,10 +1,27 @@
-import { Stack } from 'expo-router';
+import { Tabs } from 'expo-router';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function MainLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      {/* Add other authenticated screens here */}
-    </Stack>
+     <Tabs screenOptions={{ headerShown: false }} >
+        <Tabs.Screen 
+          name="index"   
+          options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}/>
+        <Tabs.Screen 
+          name="groups"   
+          options={{
+          title: 'Groups',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="group" color={color} />,
+        }}/>
+        <Tabs.Screen 
+          name="payments"   
+          options={{
+          title: 'Payments',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="money" color={color} />,
+        }}/>
+      </Tabs>
   );
 }
