4bd5de64fea6ec06cb75d96e8f9f3e2284b9be21
Ashwin Menon <56076713+Ashmn07@users.noreply.github.com>
2025-03-31 12:24:45 +0530

    Added pie chart in home page (#34)

--- frontend/app/(main)/index.tsx
+++ frontend/app/(main)/index.tsx
@@ -1,10 +1,11 @@
-import { Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Avatar } from 'react-native-elements';
+import ExpensePieChart from '../components/ExpensePieChart';
 
 export default function HomeScreen() {
   return (
-    <View>
+    <ScrollView style={{ flex: 1 }}>
       <View className="flex-row items-center justify-between border-b border-gray-200 px-4 py-3">
         <Text className="text-2xl font-bold">ExpenseBuddy</Text>
         <View className='flex-row items-center gap-3'>
@@ -19,7 +20,9 @@ export default function HomeScreen() {
         </View>
       </View>
       
-    </View>
+      <ExpensePieChart/>
+
+    </ScrollView>
     
   );
 }
